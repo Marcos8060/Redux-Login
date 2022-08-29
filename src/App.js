8350389cedcb0635043167a90a94e8c22ef7af79
Marcos8060 <marcosgav80@gmail.com>
2022-08-29 14:34:28 +0300

    installed react hook form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+
 
 function App() {
   return (
     <>
-        Hello world
+        <Router>
+          <Routes>
+            <Route path='/' element={<Register />} />
+            <Route path='/login' element={<Login />} />
+          </Routes>
+        </Router>
     </>
   );
 }
